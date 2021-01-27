5fee848b45941d528a858d487e07b0535e7058c6
su-mahesh <maheshkangude@gmail.com>
2021-01-27 16:18:24 +0530

    ADDED: HotelReservation class

---
+++ src/main/java/com/bidgelabz/HotelReservationSystem.java
@@ -0,0 +1,10 @@
+package com.bidgelabz;
+
+import java.util.HashMap;
+
+public class HotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel reservation program");
+    }
+}
